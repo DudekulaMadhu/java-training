aa339dc89f7be0b9413b4bd084faaab86799bfe0
Madhu Dudekula <m.dudekula1@zensar.com>
2022-04-22 17:36:54 +0530

    conditionalStatement

---
+++ Basics/conditionalStatements.java
@@ -0,0 +1,13 @@
+package Basics;
+
+public class conditionalStatements {
+    public static void main(String[] args){
+        int value = 500;
+        if(value > 200 || value <=500){
+            System.out.println("Item is Expensive");
+        }
+        else {
+            System.out.println("Item is Cheap");
+        }
+    }
+}
