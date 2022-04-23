a9739c159d08a05ebfa6ee33c59ec7dfb7260a30
Madhu Dudekula <m.dudekula1@zensar.com>
2022-04-23 14:49:00 +0530

    userInput

---
+++ userInput.java
@@ -0,0 +1,11 @@
+import java.util.Scanner; //import
+
+public class userInput {
+    public static void main(String[] args){
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter a number");
+            int n = sc.nextInt(); // input
+            System.out.println("You entered = " + n);
+        }
+    }
+}
