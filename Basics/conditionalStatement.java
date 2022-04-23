3ddf22f46bfc4965b038466663480971858b9b6e
Madhu Dudekula <m.dudekula1@zensar.com>
2022-04-23 13:10:33 +0530

    text

--- Basics/conditionalStatement.java
+++ Basics/conditionalStatement.java
@@ -1,8 +1,10 @@
+
+
 public class conditionalStatement {
     public static void main(String[] args){
         int value = 500;
         if(value > 200 || value <= 500){
-            System.out.println("Item is Expensive");
+            System.out.println("Item is Exoensive");
         }else{
             System.out.println("Item is Cheap");
         }
