09c74e6cfd4894a110718b9e168e4322a93a8655
Madhu Dudekula <m.dudekula1@zensar.com>
2022-04-23 13:57:16 +0530

    conditionalStatement

---
+++ conditionalSatement.java
@@ -0,0 +1,10 @@
+public class conditionalSatement {
+    public static void main(String[] args){
+        int value = 500;
+        if(value > 200 || value <= 500){
+            System.out.println("Item is Exjpensive");
+        }else{
+            System.out.println("Item is Cheap");
+        }
+    }
+}
