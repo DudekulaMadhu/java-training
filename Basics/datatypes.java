a153cc5591c3cd503356b2b75de299325945c65d
Madhu Dudekula <m.dudekula1@zensar.com>
2022-04-23 08:58:55 +0530

    text

---
+++ Basics/datatypes.java
@@ -0,0 +1,20 @@
+package Basics;
+
+public class datatypes {
+    public static void main(String[] args){
+        // numbers -> Integers
+        int a = -12;
+
+        // point, decimal value
+        double d = 45.6;
+
+        // word, sentance
+        String s = "Madhu is a developer";
+
+        //character
+        char ch ='a';
+
+        //true, false
+        boolean bool = true;
+    }
+}
