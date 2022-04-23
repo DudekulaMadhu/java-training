57667218c33a5a40d1203db3e4fbd2c29113fb9e
Madhu Dudekula <m.dudekula1@zensar.com>
2022-04-23 15:18:12 +0530

    adult

---
+++ adult.java
@@ -0,0 +1,12 @@
+public class adult {
+    public static void checkForAdult(int age){
+        if(age >= 18){
+            System.out.println("Madhu is an adult");
+        }else{
+            System.out.println("Naveen is an adult");
+        }
+    }
+    public static void main(String[] args){
+        checkForAdult(18);
+    }
+}
