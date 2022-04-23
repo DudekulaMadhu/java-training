25fc4a721f04ee26a716814ed2dbf8bd57954062
Madhu Dudekula <m.dudekula1@zensar.com>
2022-04-23 15:09:32 +0530

    datatypes

---
+++ datatypes.java
@@ -0,0 +1,14 @@
+public class datatypes {
+    public static void main(String[] args){
+        // numbers -> Integers
+        int i = 45;
+        //point -> Decimals
+        double d = 7.8;
+        //wrod -> Sentance
+        String s = "Aryan is the Trainer";
+        //Character
+        char ch = 'a';
+        // true,false
+        boolean bool = true;
+    }
+}
