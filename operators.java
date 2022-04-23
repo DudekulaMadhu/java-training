6af4a9bad6a94a78368c5fb59d94ececd57b695b
Madhu Dudekula <m.dudekula1@zensar.com>
2022-04-23 14:35:07 +0530

    operators

---
+++ operators.java
@@ -0,0 +1,8 @@
+public class operators {
+    public static void main(String[] args){
+        //System.out.println(1 < 2);
+        //System.out.println(2 >= 2);
+        //System.oot.println(5 == 5);
+        // || and &&
+    }
+}
