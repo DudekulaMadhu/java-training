5a196024c1b8978dc9ad23c772cd059e129dac70
Madhu Dudekula <m.dudekula1@zensar.com>
2022-04-23 14:59:27 +0530

    evenOdd

---
+++ evenOdd.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class evenOdd {
+    public static void main(String[] main){
+        System.out.println("Enter a number");
+        try (Scanner sc = new Scanner(System.in)) {
+            int  n = sc.nextInt();
+            // n % 2 == 0 (even)
+            // n % 2 not = 0 (odd)
+            if(n % 2 == 0){
+                System.out.println(n + "is  number");
+            }else{
+                System.out.println(n + "is odd");
+            }
+        }
+    }
+}
