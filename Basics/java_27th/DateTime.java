f479803123d0856f0b1ba42c1e0672042a0bed79
Madhu Dudekula <m.dudekula1@zensar.com>
2022-04-27 10:06:35 +0530

    DateTime

---
+++ Basics/java_27th/DateTime.java
@@ -0,0 +1,11 @@
+package Basics.java_27th;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class DateTime {
+    public static void main(String[] args){
+        System.out.println(LocalDate.now());
+        System.out.println(LocalTime.now());
+    }
+}
