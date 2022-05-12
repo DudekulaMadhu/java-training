1cfc35fc57f7a88e933f2ef70a9a7c381dbf7d7b
Madhu Dudekula <m.dudekula1@zensar.com>
2022-05-12 16:43:43 +0530

    halfreverseeven

--- mayTest_12th/nearestPrimeNumber.java
+++ mayTest_12th/nearestPrimeNumber.java
@@ -1,27 +1,62 @@
 package mayTest_12th;
 
+import java.util.Scanner;
+
 public class nearestPrimeNumber {
-    static int prime(int n){
-        if(n % 2 != 0)
-            n -= 2;
-        else  
-            n--;
-            
-        int i, j;
-        for(i = n; i >=2; i -= 2){
-            if(i % 2 == 0)
-               continue;
-            for(j = 3; j <= Math.sqrt(i); j += 2){
-                if(i % j == 0)
-                  break;
+    public static boolean isPrime(int num) {
+        // 2 - (num-1)
+        for(int i = 2; i < num; i++) {
+            if(num % i == 0) {
+                return false;
             }
-            if(j > Math.sqrt(i))
-              return i;
         }
-        return 2;
+        return true;
     }
-    public static void main(String[] args){
-        int n = 20;
-        System.out.println(n);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int nearestPrime;
+        int n = sc.nextInt();
+
+        if(isPrime(n)) {
+            System.out.println("Nearest Prime is the number itself");
+        } else {
+            // 16
+            // 0 - 16 && 16 - ....
+            // 13 && 17
+            // nearest = 17
+            // n - 0
+            int smallPrime = -1;
+            int bigPrime = -1;
+
+            // 0 - n
+            for(int i = n; i > 0; i--) {
+                if (isPrime(i)) {
+                    smallPrime = i;
+                    break;
+                }
+            }
+            int count = n + 1;
+            while(true) {
+                if(isPrime(count)) {
+                    bigPrime = count;
+                    break;
+                }
+                count++;
+            }
+            // bigPrime - n
+            // n - smallPrime
+            int steps1 = bigPrime - n;
+            int steps2 = n - smallPrime;
+
+            if(steps1 < steps2) {
+                nearestPrime = bigPrime;
+            } else {
+                nearestPrime = smallPrime;
+            }
+            System.out.println("Nearest Prime number from " + n + " is : " + nearestPrime);
+        }
+        sc.close();
+
+
     }
 }
