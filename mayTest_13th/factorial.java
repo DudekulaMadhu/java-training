dd4556332d8f753013d2e690272b0f94434b1d7e
Madhu Dudekula <m.dudekula1@zensar.com>
2022-05-13 16:45:46 +0530

    factorial

--- mayTest_13th/factorial.java
+++ mayTest_13th/factorial.java
@@ -1,12 +1,18 @@
 package mayTest_13th;
 
+import java.util.Scanner;
+
 public class factorial {
     public static void main(String[] args){
-        int i, fact = 1;
-        int number = 4;
-        for(i=1 ;i<=number; i++){
-            fact = fact * i;
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter the numer: ");
+            int number = sc.nextInt();
+            int i=1,fact=1;
+            while(i<=number){
+                fact = fact * i;
+                i++;
+            }
+            System.out.println("factorial of the number : " + fact);
         }
-        System.out.println("factorial of" + number + "is " + fact);
     }
 }
